4f22d087ea2b47fb1faef52e7a89eeaec9a85e8a
Chisan Ahn <chisan2001@naver.com>
2021-11-09 19:48:28 +0900

    Initial commit

---
+++ src/main/java/com/m1s/m1sserver/User.java
@@ -0,0 +1,2 @@
+package com.m1s.m1sserver;public class User {
+}
---
+++ src/test/java/com/m1s/m1sserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1s.m1sserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
